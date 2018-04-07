a6b2cfa1142da26f435e977024a694c2d572d21f
Mohamed Ahmed <mohamed.nurhussien@gmail.com>
2018-04-07 11:04:54 -0400

    Changing pages to static_pages and about page to help page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
-  get 'about', to: 'pages#about'
+  root to: 'static_pages#home'
+  get 'help', to: 'static_pages#about'
 end
