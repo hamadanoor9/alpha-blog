aba978255cfd0cf5355bdbba02295a6008185be4
Mohamed Ahmed <mohamed.nurhussien@gmail.com>
2018-04-07 11:28:33 -0400

    root to application#home and rendering hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#home'
+  root to: 'application#hello'
   get 'help', to: 'static_pages#about'
+  get 'home', to: 'static_pages#home'
 end
